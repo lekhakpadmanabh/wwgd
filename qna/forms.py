30f2b84db90f39058f4381af347882e7c36789cc
Padmanabh <prabhas.padmanabh@gmail.com>
2014-04-03 14:42:10 +0530

    added summernote to question submit form

--- qna/forms.py
+++ qna/forms.py
@@ -1,10 +1,16 @@
 from django.forms import ModelForm
 from .models import  Question, Answer, Vote, AnswerVote
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 class QuestionForm(ModelForm):
+    detail = forms.CharField(widget = SummernoteWidget())
     class Meta:
         model = Question
         exclude = ('submitter','rank_score')
+    def __init__(self, *args, **kwargs):
+        super(QuestionForm,self).__init__(*args,**kwargs)
+        self.fields['title'].widget.attrs.update({'class':'form-control','placeholder':'Succint title for your question...'})
+        self.fields['tags'].widget.attrs.update({'class':'form-control','placeholder':'Enter one or more tags as csv(required)'})
 
 
 class AnswerForm(ModelForm):
